38d354c7df0886103cf3bad5e403a5f2a138491b
Tom Wiesing <tkw01536@gmail.com>
2018-03-24 10:38:08 +0100

    Optionally read creds from env

--- getRECd-backend/getrecd/credentials.py
+++ getRECd-backend/getrecd/credentials.py
@@ -3,9 +3,10 @@ from spotipy.oauth2 import SpotifyClientCredentials
 try:
     from .oauth import client_id, client_secret, ibm_user, ibm_pass
 except ImportError:
-    client_id = None
-    client_secret = None
-    ibm_user = None
-    ibm_pass = None
+    import os
+    client_id = os.environ.get('SPOTIFY_CLIENT_ID')
+    client_secret = os.environ.get('SPOTIFY_CLIENT_SECRET')
+    ibm_user = os.environ.get('IBM_USER')
+    ibm_pass = os.environ.get('IBM_PASS')
 credentials = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
 ibm = {'u':ibm_user, 'p':ibm_pass}
